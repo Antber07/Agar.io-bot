30429ab6d82eea51551d14a7013d76217dbcfb6e
Antber07 <anton@goku.se>
2015-12-13 15:39:15 +0100

    Update loader.user.js

--- loader.user.js
+++ loader.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name        AposLoader
-// @namespace   AposLoader
-// @description Grabs latest versions of the bot scripts automatically.
+// @name        Anton
+// @namespace   Anton
+// @description
 // @include     http://agar.io/
 // @version     1.3
 // @grant       none
